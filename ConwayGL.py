ffd46eda0eb314e4a8d8c69c6c4fe8ea8537e729
Özgür Bulut <ozgurbulut0@outlook.com>
2019-11-20 01:27:18 +0300

    Update ConwayGL.py

--- ConwayGL.py
+++ ConwayGL.py
@@ -1,4 +1,5 @@
 import turtle
+import copy
 import random
 screen=turtle.Screen()
 turtle.setup(1000,1000)
@@ -11,13 +12,24 @@ hucreturtle.hideturtle()
 hucreturtle.speed(0)
 hucreturtle.color('blue')
 
-n = 15 # Orneklere bakilarak 15x15 kare secildi.
+n = 25 # 25x25 kare
 def cizgi(yatay_b,dikey_b,yatay_s,dikey_s):
     turtle.up()
     turtle.goto(yatay_b,dikey_b)
     turtle.down()
     turtle.goto(yatay_s,dikey_s)
-    
+def hucre_ciz(x,y):
+    lx = 600/n*x - 300  #ekran boyutlari
+    ly = 600/n*y - 300
+    kareyap(lx+1,ly+1,600/n-2)
+
+def kural_1_hesapla(x,y): 
+    sayac = 0
+    for i in range(max(x-1,0),min(x+1,n-1)+1):
+        for j in range(max(y-1,0),min(y+1,n-1)+1):
+            sayac += hucre[i][j]
+    return sayac - hucre[x][y]
+
 def izgara_ciz(): 
     turtle.pencolor('black')
     turtle.pensize(1)
@@ -40,7 +52,7 @@ def hucre_cagir():
             else:
                 hucrerow.append(0) 
         hucre.append(hucrerow) 
-def kareyap(x,y,size): 
+def kareyap(x,y,size): #Olusturulacak hucrenin boyut hareket kurallari
     hucreturtle.up()
     hucreturtle.goto(x,y)
     hucreturtle.down()
@@ -51,19 +63,32 @@ def kareyap(x,y,size):
         hucreturtle.left(90)
     hucreturtle.end_fill()
 
-def hucre_ciz(x,y):
-    lx = 600/n*x - 300  
-    ly = 600/n*y - 300
-    kareyap(lx+1,ly+1,600/n-2)
+def guncelle():#Yeni pozisyon gosterimi bu fonksiyon ile saglaniyor
+    global hucre 
+    yeninesne = copy.deepcopy(hucre)
+    for i in range(n):
+        for j in range(n):
+            k = kural_1_hesapla(i,j)
+            if k < 2 or k > 3:
+                yeninesne[i][j] = 0
+            elif k == 3:
+                yeninesne[i][j] = 1
+    hucre = copy.deepcopy(yeninesne)#Kopya nesneler uretiyoruz
+    hucreturtle.clear() 
+    tamamini_ciz()
+    screen.update() 
+    screen.ontimer(guncelle,10)
 
 def tamamini_ciz(): # Butun hucrelerin son hali
     global hucre
     for i in range(n):
         for j in range(n):
             if hucre[i][j] == 1: hucre_ciz(i,j)
-izgara_ciz()
-while(1):
-	hucre_cagir()
-	tamamini_ciz()
-	screen.update()
+    
 
+while(1):
+	izgara_ciz()
+	hucre_cagir()
+	#tamamini_ciz()
+	guncelle()
+	#screen.update()
